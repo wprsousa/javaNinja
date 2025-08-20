db246b3be3dd6684dd51feba601c5f12098cdb42
Wellington Pedro <wprsousa@gmail.com>
2025-08-20 09:49:06 -0300

    - Add `.gitignore` file with entries for common IDEs and OS artifacts - Add initial implementation of `DadosPrimitivos` class - Add IntelliJ module file `JavaNinja.iml`

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,16 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados Primitivos - int, double, float, char, boolean, short
+        * Objetivo da aula: Criar um ninja - Naruto -
+         */
+
+        int idade = 16; // Valor máximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L; // Valor máximo: 9 223 372 036 854 775 807
+    }
+}
